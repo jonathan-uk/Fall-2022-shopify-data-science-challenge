04dffef1fce2e7279977233febf548cd82d418df
jonathan_uk <86266982+jonathan-uk@users.noreply.github.com>
2022-06-13 07:29:05 +0100

    Update challenge 2.sql

--- challenge 2.sql
+++ challenge 2.sql
@@ -6,8 +6,6 @@ a. How many orders were shipped by Speedy Express in total?
 b. What is the last name of the employee with the most orders?
 c. What product was ordered the most by customers in Germany?
 
-https://www.w3schools.com/SQL/TRYSQL.ASP?FILENAME=TRYSQL_SELECT_ALL
-*/
 
 -- a. How many orders were shipped by Speedy Express in total?
 
